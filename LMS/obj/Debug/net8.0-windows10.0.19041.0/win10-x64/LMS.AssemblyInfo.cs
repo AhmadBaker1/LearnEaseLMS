418c498ecaf5913fe472f5dbf6c087e0897a6f9a
RamseyKHX <rameez.ahmed@edu.sait.ca>
2024-04-22 15:36:59 -0600

    Changes in Courses-Search

--- LMS/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/LMS.AssemblyInfo.cs
+++ LMS/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/LMS.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LMS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+aa6560812f5dd644c0cedab5e8292023ea958e0f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+210ac47d9b073d0e10200e6b311696598ff630f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("LMS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LMS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
